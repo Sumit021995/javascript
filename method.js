701cd5d023a0c7de07ce447f4dcf51930b52c63f
sumit021995 <kaku.saurav@gmail.com>
2023-10-12 23:54:22 +0530

    first letter upper

--- method.js
+++ method.js
@@ -1,5 +1,22 @@
 function replaceUpperCase(str){
-  // let a = String(str);
     return str.replace(/a/g, 'm').toUpperCase();
 }
 console.log(replaceUpperCase("sumit saurav"));
+
+function firstLetterUpperCase(str){
+  let b = str[0].toUpperCase();
+  console.log(b);
+  let a = str.slice(1);
+  console.log(a);
+  let c = b.concat(a);
+  console.log(c);
+  
+
+}
+firstLetterUpperCase("my name is sumit")
+
+function firstLetterUpper(str){
+  return str[0].toUpperCase()+str.slice(1); 
+}
+
+console.log(firstLetterUpper('sumit is my name'));
