24b5413d3f779045fb5b4eb877373724c8ad132a
sumit021995 <kaku.saurav@gmail.com>
2023-10-21 01:02:35 +0530

    Object.assign()

--- objectmethod2.js
+++ objectmethod2.js
@@ -4,3 +4,36 @@ tinderUser.id = "123abc"
 tinderUser.name = "Rohit"
 tinderUser.isLoggedIn = false
  console.log(tinderUser); // { id: '123abc', name: 'Rohit', isLoggedIn: false }
+
+
+ let regularUser = {
+    email:'some@gmail.com',
+    fullname: {
+        userfullname:{
+            firstname:'Sumit'
+        }
+    }
+ }
+ console.log(regularUser);
+ /*
+
+ {
+  email: 'some@gmail.com',
+  fullname: { userfullname: { firstname: 'Sumit' } }
+}
+
+  */
+ console.log(regularUser.fullname.userfullname);//{ firstname: 'Sumit' }
+
+ let obj1 = {1:'a',2:'b'}
+ let obj2 = {3:'a',4:'b'}
+
+let obj3 = { obj1 , obj2 }
+console.log(obj3);//{ obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } }
+
+let obj4 = Object.assign(obj1,obj2)
+console.log(obj4); // { '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
+
+let obj5 = Object.assign({},obj1,obj2);
+console.log(obj5); // { '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
+ //Combine objects
