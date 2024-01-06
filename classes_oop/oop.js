ccdfe6a402078c23bcd71262fe6884a489355a51
sumit021995 <kaku.saurav@gmail.com>
2024-01-06 22:50:02 +0530

    oop file updated

---
+++ classes_oop/oop.js
@@ -0,0 +1,12 @@
+const user = {
+    username: "Sumit",
+    loginCount: 8,
+    signedIn: true,
+
+    getUserDetails: function(){
+        //console.log("Got user details from database");
+        // console.log(`Username: ${this.username}`);
+        console.log(this);
+    }
+
+}
