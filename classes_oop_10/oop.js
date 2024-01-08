03bef2be68fbc4361db1bdab3a685fb31013c041
sumit021995 <kaku.saurav@gmail.com>
2024-01-08 21:57:27 +0530

    oops.js file updated

--- classes_oop_10/oop.js
+++ classes_oop_10/oop.js
@@ -6,12 +6,19 @@ const user = {
     signedIn: true,
 
     getUserDetails: function(){
-        console.log("Got user details from database");
-        // console.log(`Username: ${this.username}`);
-        console.log(this);
+        console.log("Got user details from database"); // Got user details from database
+        console.log(`Username: ${this.username}`); // Username: Sumit
+        console.log(this);/* {
+            username: 'Sumit',
+            loginCount: 8,
+            signedIn: true,
+            getUserDetails: [Function: getUserDetails]
+          }
+          undefined */
     }
 
 }
 
 console.log(user.username); // Sumit
-console.log(user.getUserDetails());
+console.log(user.getUserDetails()); 
+
