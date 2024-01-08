70310eff41ff8f37cc0dd1f99f5abb7f8cc7e61f
sumit021995 <kaku.saurav@gmail.com>
2024-01-08 23:51:51 +0530

    index.js file updated

--- api-project/index.js
+++ api-project/index.js
@@ -1,43 +1,48 @@
 let myData;
+let url = 'https://jacintodesign.github.io/quotes-api/data/quotes.json'
 
-document.getElementById("start").addEventListener("click", () => {
-  fetch("https://jacintodesign.github.io/quotes-api/data/quotes.json")
+// document.getElementById("start").addEventListener("click", () => {
+//   fetch("https://jacintodesign.github.io/quotes-api/data/quotes.json")
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     myData = this.data;
+// console.log(data);
+//   console.log(typeof data); // object
+//   console.log(data.length); // 8261
+//   console.log(data[0].text); // Today is the tomorrow we worried about yesterday.
+//     let randomNo = parseInt(Math.random() * 8261 + 1);
+//   console.log(randomNo);
+//     return data[randomNo].text;
+//   })
+//   .then((text) => {
+//     document.getElementById("outputText").innerText = `${text}`;
+//   })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+//   });
+
+async function getMyDetails (){
+  try {
+    fetch()
+  } catch (error) {
+    
+  }
+}
+
+fetch("url")
   .then((response) => {
+    console.log(response.json());
     return response.json();
+  }).then((data)=>{
+    myData = data;
+    console.log(myData);
   })
-  .then((data) => {
-    myData = this.data;
-    // console.log(data);
-    //   console.log(typeof data); // object
-    //   console.log(data.length); // 8261
-    //   console.log(data[0].text); // Today is the tomorrow we worried about yesterday.
-    let randomNo = parseInt(Math.random() * 8261 + 1);
-    //   console.log(randomNo);
-    return data[randomNo].text;
-  })
-  .then((text) => {
-    document.getElementById("outputText").innerText = `${text}`;
-  })
-    .catch((error) => {
-      console.log(error);
-    });
+ .catch((error) => {
+    console.log(error);
   });
-  document.getElementById("outputText").innerText = `${myData[parseInt(Math.random() * 8261 + 1)]}`;
-  
-  //   fetch('https://jacintodesign.github.io/quotes-api/data/quotes.json')
-  //   .then((response)=>{
-    //     return response.json()
-    //   }).then((data)=>{
-      //     // console.log(data);
-      //     console.log(typeof data); // object
-      //     console.log(data.length); // 8261
-      //     console.log(data[0].text); // Today is the tomorrow we worried about yesterday.
-      //     let randomNo= parseInt(Math.random()*8261 + 1);
-      //     console.log(randomNo);
-//     return data[randomNo].text
-//   }).then((text)=>{
-//         document.body.innerHTML = `<div>${text}</div>`
-//   })
-//   .catch((error)=>{
-//     console.log(error);
-//   })
+
+// document.getElementById("outputText").innerText =`${myData[parseInt(Math.random()*8261 + 1)]}`;
+console.log(myData);
