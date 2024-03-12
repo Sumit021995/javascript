6e878b91ec1d450109b72b15ebd483548fc28be0
sumit021995 <kaku.saurav@gmail.com>
2024-03-12 21:14:41 +0530

    file updated

--- classes_oop_10/new&prototype.js
+++ classes_oop_10/new&prototype.js
@@ -3,12 +3,15 @@ const multiplyBy5 = (num)=>{
 }
 
 multiplyBy5.power = 2
+// const new = multiplyBy5.prototype
+
 
 console.log(multiplyBy5(3)); // 15
 console.log(multiplyBy5.power); // 2
 console.log(multiplyBy5.Prototype); // undefined , {}
+// console.log(new); // undefined , {}
 
-function createItemPrice(item,score){
-    this.item = item
-    this.score = score
-}
+// function createItemPrice(item,score){
+//     this.item = item
+//     this.score = score
+// }
