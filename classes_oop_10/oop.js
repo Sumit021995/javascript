c0ebea953469a093bad5a97d75ebc50ade9223d5
sumit021995 <kaku.saurav@gmail.com>
2024-03-12 13:12:35 +0530

    file updated

--- classes_oop_10/oop.js
+++ classes_oop_10/oop.js
@@ -1,26 +1,26 @@
 // object literal
 
-const user = {
-    username: "Sumit",
-    loginCount: 8,
-    signedIn: true,
+// const user = {
+//     username: "Sumit",
+//     loginCount: 8,
+//     signedIn: true,
 
-    getUserDetails: function(){
-        console.log("Got user details from database"); // Got user details from database
-        console.log(`Username: ${this.username}`); // Username: Sumit
-        console.log(this);/* {
-            username: 'Sumit',
-            loginCount: 8,
-            signedIn: true,
-            getUserDetails: [Function: getUserDetails]
-          }
-          undefined */
-    }
+//     getUserDetails: function(){
+//         console.log("Got user details from database"); // Got user details from database
+//         console.log(`Username: ${this.username}`); // Username: Sumit
+//         console.log(this);/* {
+//             username: 'Sumit',
+//             loginCount: 8,
+//             signedIn: true,
+//             getUserDetails: [Function: getUserDetails]
+//           }
+//           undefined */
+//     }
 
-}
+// }
 
-console.log(user.username); // Sumit
-console.log(user.getUserDetails()); 
+// console.log(user.username); // Sumit
+// console.log(user.getUserDetails()); 
 
 // "this" keyword is used for this current context
 
